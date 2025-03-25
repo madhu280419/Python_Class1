a133a593d2dec9092cd38729d71bd76fc9d777ed
Madhu babu Yalavarthi <madhuyalavarthi1027@gmail.com>
2025-03-25 18:22:48 -0500

    ..

---
+++ pos_neg.py
@@ -0,0 +1,7 @@
+number = float(input("enter a number"))
+if number > 0:
+    print("the number is positive :", number)
+elif number < 0:
+    print("the number is negative:", number)
+else:
+    print("the number is zero")
