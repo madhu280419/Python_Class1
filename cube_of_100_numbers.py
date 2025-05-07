7c103b9cc7c01735a9230b5f65dec633ec99f0d0
Madhu babu Yalavarthi <madhuyalavarthi1027@gmail.com>
2025-05-07 18:14:23 -0500

    cube of 100 numbers

---
+++ cube_of_100_numbers.py
@@ -0,0 +1,19 @@
+#Cube of 100 numbers
+def main():
+    print("The cube of numbers 1 to 100:")
+    for num in range(1,101):
+        cube = num ** 3
+        print(f"{num} = {cube}")
+if __name__ == "__main__":
+    main()
+    
+    
+#square of numbers
+def main():
+    print("The square of numbers 1 to 100:")
+    for num in range (1,101):
+        square = num ** 2
+        print(f"{num} = {square}")
+if __name__ == "__main__":
+    main()
+        
